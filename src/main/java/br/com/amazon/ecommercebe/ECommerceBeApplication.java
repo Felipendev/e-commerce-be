77c460cf75e434e4c77e59e128b25a27a5344ef1
Felipendev <felipe.psic@outlook.com.br>
2022-08-11 14:58:21 -0300

    implementa API de teste, ok

--- src/main/java/br/com/amazon/ecommercebe/ECommerceBeApplication.java
+++ src/main/java/br/com/amazon/ecommercebe/ECommerceBeApplication.java
@@ -2,9 +2,19 @@ package br.com.amazon.ecommercebe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ECommerceBeApplication {
+
+	@GetMapping
+	public String getHomeTeste() {
+		return "E-commerce - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceBeApplication.class, args);
