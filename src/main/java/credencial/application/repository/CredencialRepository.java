1ead1427db7f6f39c17acf74cbcb132715df2195
Felipendev <felipe.psic@outlook.com.br>
2022-08-12 16:21:33 -0300

    remove arquivos de security que nao serao usados agora

--- src/main/java/credencial/application/repository/CredencialRepository.java
+++
@@ -1,8 +0,0 @@
-package credencial.application.repository;
-
-import credencial.domain.Credencial;
-
-public interface CredencialRepository {
-	Credencial salva(Credencial credencial);
-	Credencial buscaCredencialPorUsuario(String usuario);
-}
--- src/main/java/usuario/application/service/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package usuario.application.service;
-
-import usuario.domain.Usuario;
-
-public interface UsuarioRepository {
-	Usuario salva(Usuario usuario);
-}
