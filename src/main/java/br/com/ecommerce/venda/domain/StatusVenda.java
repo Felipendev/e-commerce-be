1aab1e449ec345a5d32343f2d2b3cf8f7d0106dc
Felipendev <felipe.psic@outlook.com.br>
2022-08-12 17:03:13 -0300

    implementa dominio da entidade venda

---
+++ src/main/java/br/com/ecommerce/venda/domain/StatusVenda.java
@@ -0,0 +1,5 @@
+package br.com.ecommerce.venda.domain;
+
+public enum StatusVenda {
+    ATIVA,FINALIZADA,CANCELADA
+}
---
+++ src/main/java/br/com/ecommerce/venda/domain/Venda.java
@@ -0,0 +1,30 @@
+package br.com.ecommerce.venda.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Venda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid",name = "id", updatable = false, unique = true, nullable = false)
+    private UUID idVenda;
+    @NotBlank
+    private String nomeCliente;
+    @NotBlank
+    private String valorVenda;
+    @NotNull
+    private LocalDateTime dataVenda;
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private StatusVenda statusVenda;
+}
