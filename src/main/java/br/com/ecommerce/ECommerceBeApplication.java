691fb91f70d90d4b08aecc5022d7ebfcb34d9f17
Felipendev <felipe.psic@outlook.com.br>
2022-08-12 16:48:31 -0300

    renomeia aplicacao

--- src/main/java/br/com/ecommerce/ECommerceBeApplication.java
+++ src/main/java/br/com/ecommerce/ECommerceBeApplication.java
@@ -1,4 +1,4 @@
-package br.com.amazon.ecommercebe;
+package br.com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/ecommerce/ECommerceBeApplicationTests.java
+++ src/test/java/br/com/ecommerce/ECommerceBeApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.amazon.ecommercebe;
+package br.com.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
