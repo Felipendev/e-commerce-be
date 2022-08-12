84fe5b82d6ec60bf1012b1b420e8fe9a26eea880
Felipendev <felipe.psic@outlook.com.br>
2022-08-12 17:16:32 -0300

    implementa request e response de venda

--- src/main/java/br/com/ecommerce/venda/application/api/VendaRequest.java
+++ src/main/java/br/com/ecommerce/venda/application/api/VendaRequest.java
@@ -1,4 +1,20 @@
 package br.com.ecommerce.venda.application.api;
 
+import br.com.ecommerce.venda.domain.StatusVenda;
+import lombok.Value;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Value
 public class VendaRequest {
+    @NotBlank
+    private String nomeCliente;
+    @NotBlank
+    private String valorVenda;
+    @NotNull
+    private LocalDateTime dataVenda;
+    @NotNull
+    private StatusVenda statusVenda;
 }
--- src/main/java/br/com/ecommerce/venda/application/api/VendaResponse.java
+++ src/main/java/br/com/ecommerce/venda/application/api/VendaResponse.java
@@ -1,4 +1,12 @@
 package br.com.ecommerce.venda.application.api;
 
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
+@Builder
 public class VendaResponse {
+    private UUID idVenda;
 }
